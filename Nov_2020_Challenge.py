58e58b266bde624f11a42b23ce43b44342bd933f
Jan Michael Cayabyab Austria <55464895+janmichael88@users.noreply.github.com>
2020-11-11 00:13:54 -0800

    Add files via upload

--- Nov_2020_Challenge.py
+++ Nov_2020_Challenge.py
@@ -839,3 +839,440 @@ class Solution(object):
             return temp
         return dfs(head)
         
+
+####################
+#Binary Tree Tilt
+###################
+#so close...
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        make the tree first and then sum up all the nodes, any of the three traversals will work
+        '''
+        def dfs(node):
+            if not node:
+                return
+            if not node.left and not node.right:
+                return 0
+            if node.left:
+                sum_left = 0
+                sum_left += node.val
+                dfs(node.left)
+                dfs(node.right)
+            if node.right:
+                sum_right = 0
+                sum_right += node.val
+                dfs(node.left)
+                dfs(node.right)
+            
+            node.left = ListNode(val=sum_left)
+            node.right = ListNode(val=sum_right)
+            node = abs(node.left.val - node.right.val)
+            
+        #invoke in place
+        dfs(root)
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        to get the tilt at each node, we need the sum of its left and right subtrees
+        tilt(node) = sum(node.left) + sum(node.right)
+        and 
+        sum(node) = node.val + sum(node.left) + sum(node.right)
+        '''
+        self.answer = 0
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+            tilt = abs(left_sum-right_sum)
+            self.answer += tilt
+            
+            return node.val +left_sum+right_sum
+        
+        dfs(root)
+        return self.answer
+
+#####################
+# Symmetric Tree
+#####################
+#so clsoe 119/165
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        '''
+        a subtree is symmetric if at a node
+        the node.left == node.right and node.right == node.left
+        '''
+        if not root:
+            return True
+        def dfs(node):
+            if not node:
+                return
+            if node.left == node.right and node.right == node.left:
+                return True
+            if dfs(node.left) and dfs(node.right):
+                return True
+            return False
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        '''
+        a subtree is symmetric if at a node
+        the node.left == node.right and node.right == node.left
+        use two poiners and compare subtrees
+        '''
+        if not root:
+            return True
+        def dfs(node1,node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            return (node1.val == node2.val) and dfs(node1.left,node2.right) and dfs(node1.right,node2.left)
+        
+        return dfs(root,root)
+        
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        '''
+        iteratively?
+        use q, add in both roots and check if left == right, right == left
+        '''
+        q = deque([])
+        q.append(root)
+        q.append(root)
+        while q:
+            node1 = q.popleft()
+            node2 = q.popleft()
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+            #add the children
+            q.append(node1.left)
+            q.append(node2.right)
+            q.append(node1.right)
+            q.append(node2.left)
+        
+        return True
+
+
+#################################################
+#   Maximum Difference Between Node and Ancestor
+#################################################
+# # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        well the hint gave it away...LOL
+        for each node find the min and max of its decnedants
+        the min would be all the way to the left, and the max would be all the way to the right
+        update a self variable outside the recursive call
+        '''
+        self.max = float('-inf')
+        
+        def find_min_max(node):
+            if not node:
+                return
+            L = node.left
+            R = node.right
+            while L:
+                if L:
+                    L = L.left
+            mini = L.val
+            while R.right:
+                R = R.right
+            maxi = R.val
+            return abs(mini-maxi)
+        
+        def dfs(node):
+            if not node:
+                return
+            self.max = max(self.max,find_min_max(node))
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        brute force recursion
+        we can compare every desendant of a node, and just update a max
+        lets define our helper function, which taks in a node and two inters which are the max and min
+        the function will update outside its scope
+        we are really just going down all paths ane keeping track of a max and min along a path
+        at the end we update the largest difference
+        '''
+        if not root:
+            return 0
+        
+        self.result = float('-inf')
+        def dfs(node, mini,maxi):
+            if not node:
+                return
+            #update
+            self.result = max(self.result,abs(node.val - mini), abs(node.val- maxi))
+            mini = min(node.val,mini)
+            maxi = max(node.val,maxi)
+            dfs(node.left,mini,maxi)
+            dfs(node.right,mini,maxi)
+            
+        dfs(root,root.val,root.val)
+        return self.result
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        brute force recursion
+        we can compare every desendant of a node, and just update a max
+        lets define our helper function, which taks in a node and two inters which are the max and min
+        the function will update outside its scope
+        '''
+        if not root:
+            return 0
+        
+        def dfs(node, mini,maxi):
+            if not node:
+                return maxi - mini
+            #update
+            mini = min(node.val,mini)
+            maxi = max(node.val,maxi)
+            left = dfs(node.left,mini,maxi)
+            right = dfs(node.right,mini,maxi)
+            return max(left,right)
+            
+        return dfs(root,root.val,root.val)
+            
+            
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        '''
+        iterative dfs
+        https://www.youtube.com/watch?v=f37BCBHGFGA&ab_channel=RenZhang
+        '''
+        result = 0
+        q = [(root,root.val,root.val)]
+        while q:
+            node, cur_min,cur_max = q.pop()
+            cur_min = min(cur_min,node.val)
+            cur_max = max(cur_max,node.val)
+            for child in [node.left,node.right]:
+                if not child:
+                    continue
+                q.append((child,cur_min,cur_max))
+            #at leaf
+            if not node.left or node.right:
+                result = max(result,cur_max-cur_min)
+                
+        return result
+
+################################
+# 172. Factorial Trailing Zeroes
+################################
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        '''
+        the naive way is to just compute and count up the leading zeros
+        '''
+        product = 1
+        while n >= 1:
+            product *= n
+            n -= 1
+        
+        zeros = 0
+        while product % 10 == 0:
+            zeros += 1
+            product //= 10
+        return zeros
+        
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        '''
+        anytime i multiple by 10, we increase our zero count by 1
+        so the quesiton becomes, how many times do we multiply by 10?
+        well we can count up the pairs of 2s and 5s 
+        and take the min of either 2 or 5
+        in pseudo code:
+        twos = 0
+        for i in range(1,n):
+            if i % 2 == 0:
+                twos += 1
+        fives = 0
+        for i in range(1,n):
+            if i % 5 == 0:
+                fives += 1
+        return min(twos,fives)
+        but what is we have factors that contain multiples 5s and twos, well we just keep dividing until we can't anymore
+        twos = 0
+        for i in range(1,n):
+            while i % 2 == 0:
+                twos += 1
+                i //= 2
+        fives = 0
+        for i in range(1,n):
+            while i % 5 == 0:
+                fives += 1
+                i //= 2
+        return min(twos,fives)
+        i can remove first pass for twos because it will always be greater than 5
+        and we can finally pass in incrementes of 5
+        fives = 0
+        for i in range(5,n+1,5):
+            while i % 5 == 0:
+                fives += 1
+                i //= 5
+        return fives
+        fives = 0
+        for i in range(5,n+1,5):
+            #starting power
+            power = 5
+            while i % power == 0:
+                fives += 1
+                power *= 5
+        return fives
+        
+        how about logarithmic time
+        just keep diving n by 5 and incremding that count of zeros by how many times 5 went into the current n
+        since its integer division, n will eventually get to zero
+        at that point we just get the number after increamting
+        
+        '''
+        zeros = 0
+        while n > 0:
+            n //=5
+            zeros += n
+        return zeros
+
+
+######################
+# Flippig an Image
+######################
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        '''
+        well just reverse the rows and flips te bits
+        '''
+        rows = len(A)
+        cols = len(A[0])
+        #reverse
+        for i in range(0,rows):
+            A[i] = A[i][::-1]
+        #invert
+        for i in range(0,rows):
+            for j in range(0,cols):
+                if A[i][j] == 1:
+                    A[i][j] = 0
+                else:
+                    A[i][j] = 1
+        return A
