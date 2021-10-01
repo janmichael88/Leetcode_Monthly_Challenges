b1b0aa2f1acf5bdb24281e5612083748bcb28612
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2021-10-01 11:33:47 -0400

    Adding dp solution to LC 1143

--- Oct_2021.py
+++ Oct_2021.py
@@ -10,7 +10,7 @@ class Solution:
         if i and j match, advance them both, if they dont, there are two options
         stay at i, move j
         stat at j, move i
-        if either of these pointers got the end, we have a subsequence, which is just i-j
+        if either of these pointers got the end, we have a subsequence, which is just
         '''
         
         memo = {}
@@ -36,3 +36,24 @@ class Solution:
             return res
         
         return rec(0,0)
+
+#dp solution, 2d array
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        '''
+        dp solution, just translate from the top down recursive approach
+        remember we work backwards
+        '''
+        M = len(text1)
+        N = len(text2)
+        
+        dp = [[0]*(N+1) for _ in range(M+1)]
+        #remember we padded the dp array with one extra col and row with zeros
+        for i in range(M-1,-1,-1):
+            for j in range(N-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+        
+        return dp[0][0]
