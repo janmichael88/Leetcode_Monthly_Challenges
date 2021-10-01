4d8d0e6b3148518df31bd84f744cc85327d23577
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2021-10-01 11:19:10 -0400

    First Solution to LC 1143

--- Oct_2021.py
+++ Oct_2021.py
@@ -1,4 +1,38 @@
-'''
-01_OCT_2021
-1143. Longest Common Subsequence
-'''
+####################################
+#01_OCT_2021
+#1143. Longest Common Subsequence
+###################################
+#recursive knapsack solution
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        '''
+        i can use recursion, two pointers i and j,
+        if i and j match, advance them both, if they dont, there are two options
+        stay at i, move j
+        stat at j, move i
+        if either of these pointers got the end, we have a subsequence, which is just i-j
+        '''
+        
+        memo = {}
+        def rec(i,j):
+            #ending case
+            if i == len(text1) or j == len(text2):
+                return 0
+            #retrieve
+            if (i,j) in memo:
+                return memo[(i,j)]
+
+            #matching, 1 + advance both
+            if text1[i] == text2[j]:
+                res = 1 + rec(i+1,j+1)
+                memo[(i,j)] = res
+                return res
+            
+            #two options
+            first = rec(i+1,j)
+            second = rec(i,j+1)
+            res = max(first,second)
+            memo[(i,j)] = res
+            return res
+        
+        return rec(0,0)
