40ef4e2fbf333702c17958233d1b9a7714d76606
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2022-08-13 13:15:23 -0700

    30. Substring with Concatenation of All Words (Revisited)

--- Aug_2022.py
+++ Aug_2022.py
@@ -847,3 +847,85 @@ class Solution:
         
         inorder(root)
         return self.min_dist
+
+###########################################################
+# 30. Substring with Concatenation of All Words (Revisited)
+# 13AUG22
+############################################################
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        '''
+        first solutino involved keeping hashmap of counts of words, and trying using the words in the substring up
+        and check if we could have done this from this index
+        the only problem is that we have to recompute the hashmaps of words every time to get the counts
+        we also repeated alot because may have travesed an index already part of the substring 
+        
+        turns out we can also use a sliding window
+        instead of calling the check functions, we try to find all valid substrings in one pass using sliding iwndow
+        
+        left and right pointers, with right moving in increments of wordlength
+        if we find a word not in words, move up left past this word and start again, also if we exceed the length of the substring size
+        
+        we also need to make sure of multiplicity of each words
+        example,s = 'foofoobar'
+        words = ['foo','bar'], we need to only foo once
+        
+        this yields another criterios for moving the left bound
+            we move left until we find the exceees word and remove it
+        '''
+        n = len(s)
+        k = len(words)
+        word_length = len(words[0])
+        substring_size = word_length * k
+        word_count = collections.Counter(words)
+        
+        def sliding_window(left):
+            words_found = collections.defaultdict(int)
+            words_used = 0
+            excess_word = False
+            
+            # Do the same iteration pattern as the previous approach - iterate
+            # word_length at a time, and at each iteration we focus on one word
+            for right in range(left, n, word_length):
+                if right + word_length > n:
+                    break
+
+                sub = s[right : right + word_length]
+                if sub not in word_count:
+                    # Mismatched word - reset the window
+                    words_found = collections.defaultdict(int)
+                    words_used = 0
+                    excess_word = False
+                    left = right + word_length # Retry at the next index
+                else:
+                    # If we reached max window size or have an excess word
+                    while right - left == substring_size or excess_word:
+                        # Move the left bound over continously
+                        leftmost_word = s[left : left + word_length]
+                        left += word_length
+                        words_found[leftmost_word] -= 1
+
+                        if words_found[leftmost_word] == word_count[leftmost_word]:
+                            # This word was the excess word
+                            excess_word = False
+                        else:
+                            # Otherwise we actually needed it
+                            words_used -= 1
+                    
+                    # Keep track of how many times this word occurs in the window
+                    words_found[sub] += 1
+                    if words_found[sub] <= word_count[sub]:
+                        words_used += 1
+                    else:
+                        # Found too many instances already
+                        excess_word = True
+                    
+                    if words_used == k and not excess_word:
+                        # Found a valid substring
+                        answer.append(left)
+        
+        answer = []
+        for i in range(word_length):
+            sliding_window(i)
+
+        return answer
