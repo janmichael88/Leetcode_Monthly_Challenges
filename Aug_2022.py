0f044e00d35b13f7aa9c7ace9002046a31e759e2
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2022-08-14 23:50:01 -0700

    126. Word Ladder II (REVISITED)

--- Aug_2022.py
+++ Aug_2022.py
@@ -929,3 +929,237 @@ class Solution:
             sliding_window(i)
 
         return answer
+
+##############################
+# 126. Word Ladder II (REVISITED)
+# 14AUG22
+##############################
+#https://leetcode.com/problems/word-ladder-ii/discuss/2423485/Python-Three-steps-approach
+#https://leetcode.com/problems/word-ladder-ii/discuss/2367587/Python-BFS-%2B-DFS-With-Explanation-Why-Optimization-Is-Needed-to-Not-TLE
+#the last previous approaches get TLE because we keep going down paths that are dead ends
+class Solution:
+
+    WILDCARD = "."
+    
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        """
+        Given a wordlist, we perform BFS traversal to generate a word tree where
+        every node points to its parent node.
+        
+        Then we perform a DFS traversal on this tree starting at the endWord.
+        """
+        if endWord not in wordList:
+            # end word is unreachable
+            return []
+        
+        # first generate a word tree from the wordlist
+        word_tree = self.getWordTree(beginWord, endWord, wordList)
+        
+        # then generate a word ladder from the word tree
+        return self.getLadders(beginWord, endWord, word_tree)
+    
+    
+    def getWordTree(self,
+                    beginWord: str,
+                    endWord: str,
+                    wordList: List[str]) -> Dict[str, List[str]]:
+        """
+        BFS traversal from begin word until end word is encountered.
+        
+        This functions constructs a tree in reverse, starting at the endWord.
+        """
+        # Build an adjacency list using patterns as keys
+        # For example: ".it" -> ("hit"), "h.t" -> ("hit"), "hi." -> ("hit")
+        adjacency_list = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + Solution.WILDCARD + word[i+1:]
+                adjacency_list[pattern].append(word)
+        
+        # Holds the tree of words in reverse order
+        # The key is an encountered word.
+        # The value is a list of preceding words.
+        # For example, we got to beginWord from no other nodes.
+        # {a: [b,c]} means we got to "a" from "b" and "c"
+        visited_tree = {beginWord: []}
+        
+        # start off the traversal without finding the word
+        found = False
+        
+        q = deque([beginWord])
+        while q and not found:
+            n = len(q)
+            
+            # keep track of words visited at this level of BFS
+            visited_this_level = {}
+
+            for i in range(n):
+                word = q.popleft()
+                
+                for i in range(len(word)):
+                    # for each pattern of the current word
+                    pattern = word[:i] + Solution.WILDCARD + word[i+1:]
+
+                    for next_word in adjacency_list[pattern]:
+                        if next_word == endWord:
+                            # we don't return immediately because other
+                            # sequences might reach the endWord in the same
+                            # BFS level, this is important here
+                            found = True
+                        if next_word not in visited_tree:
+                            if next_word not in visited_this_level:
+                                visited_this_level[next_word] = [word]
+                                # queue up next word iff we haven't visited it yet
+                                # or already are planning to visit it
+                                q.append(next_word)
+                            else:
+                                visited_this_level[next_word].append(word)
+            
+            # add all seen words at this level to the global visited tree
+            visited_tree.update(visited_this_level)
+            
+        return visited_tree
+    
+    
+    def getLadders(self,
+                   beginWord: str,
+                   endWord: str,
+                   wordTree: Dict[str, List[str]]) -> List[List[str]]:
+        """
+        DFS traversal from endWord to beginWord in a given tree.
+        """
+        def dfs(node: str) -> List[List[str]]:
+            if node == beginWord:
+                return [[beginWord]]
+            if node not in wordTree:
+                return []
+
+            res = []
+            parents = wordTree[node]
+            for parent in parents:
+                res += dfs(parent)
+            for r in res:
+                r.append(node)
+            return res
+
+        return dfs(endWord)
+
+#another way
+class Solution:
+    def findLadders(
+        self, beginWord: str, endWord: str, wordList: list[str]
+    ) -> list[list[str]]:
+
+        # 1. Create adjacency list
+        def adjacencyList():
+
+            # Initialize the adjacency list
+            adj = defaultdict(list)
+
+            # Iterate through all words
+            for word in wordList:
+
+                # Iterate through all characters in a word
+                for i, _ in enumerate(word):
+
+                    # Create the pattern
+                    pattern = word[:i] + "*" + word[i + 1 :]
+
+                    # Add a word into the adjacency list based on its pattern
+                    adj[pattern].append(word)
+
+            return adj
+
+        # 2. Create reversed adjacency list
+        def bfs(adj):
+
+            # Initialize the reversed adjacency list
+            reversedAdj = defaultdict(list)
+
+            # Initialize the queue
+            queue = deque([beginWord])
+
+            # Initialize a set to keep track of used words at previous level
+            visited = set([beginWord])
+
+            while queue:
+
+                # Initialize a set to keep track of used words at the current level
+                visitedCurrentLevel = set()
+
+                # Get the number of words at this level
+                n = len(queue)
+
+                # Iterate through all words
+                for _ in range(n):
+
+                    # Pop a word from the front of the queue
+                    word = queue.popleft()
+
+                    # Generate pattern based on the current word
+                    for i, _ in enumerate(word):
+
+                        pattern = word[:i] + "*" + word[i + 1 :]
+
+                        # Itereate through all next words
+                        for nextWord in adj[pattern]:
+
+                            # If the next word hasn't been used in previous levels
+                            if nextWord not in visited:
+
+                                # Add such word to the reversed adjacency list
+                                reversedAdj[nextWord].append(word)
+
+                                # If the next word hasn't been used in the current level
+                                if nextWord not in visitedCurrentLevel:
+
+                                    # Add such word to the queue
+                                    queue.append(nextWord)
+
+                                    # Mark such word as visited
+                                    visitedCurrentLevel.add(nextWord)
+
+                # Once we done with a level, add all words visited at this level to the visited set
+                visited.update(visitedCurrentLevel)
+
+                # If we visited the endWord, end the search
+                if endWord in visited:
+                    break
+
+            return reversedAdj
+
+        # 3. Construct paths based on the reversed adjacency list using DFS
+        def dfs(reversedAdj, res, path):
+
+            # If the first word in a path is beginWord, we have succesfully constructed a path
+            if path[0] == beginWord:
+
+                # Add such path to the result
+                res.append(list(path))
+
+                return
+
+            # Else, get the first word in a path
+            word = path[0]
+
+            # Find next words using the reversed adjacency list
+            for nextWord in reversedAdj[word]:
+
+                # Add such next word to the path
+                path.appendleft(nextWord)
+
+                # Recursively go to the next word
+                dfs(reversedAdj, res, path)
+
+                # Remove such next word from the path
+                path.popleft()
+
+            # Return the result
+            return res
+
+        # Do all three steps
+        adj = adjacencyList()
+        reversedAdj = bfs(adj)
+        res = dfs(reversedAdj, [], deque([endWord]))
+
+        return res
