57b79d9dd103283a3d8aaa7a06c3705fee393aa2
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2022-09-27 15:59:10 -0400

    990. Satisfiability of Equality Equations

--- Sep_2022.py
+++ Sep_2022.py
@@ -2689,3 +2689,189 @@ class Solution:
         return ans
 
 #now using property of BSTs
+
+
+###################################
+# 990. Satisfiability of Equality Equations
+# 26SEP22
+###################################
+#close one
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        '''
+        i can traverse the equations and make mappings for each thing
+        two maps equals and not equals
+        then we can just check the validaty of the string and try to make it to the end
+        nope, its a graph problem
+        
+        dfs solution
+            built adj list of colors, of unidretec graph where each variable equalts another
+            then we dfs and color each equal, example a == b, b == c, then a == b, these three would be all the same color
+            then we traverse the equations and check for a contradiction of x != y
+        '''
+        adj_list = defaultdict(list)
+        for eq in equations:
+            if eq[1] == '=':
+                a = eq[0]
+                b = eq[-1]
+                adj_list[a].append(b)
+                adj_list[b].append(a)
+        
+        
+        #we only have 26 lower case chars, so lets try to color then
+        #use dfs to mark groupings, color to nodes
+        seen = set()
+        groups = []
+        
+        def dfs(node,comp):
+            if node not in seen:
+                seen.add(node)
+                comp.add(node)
+                for neigh in adj_list[node]:
+                    dfs(neigh,comp)
+                    
+        for node in adj_list:
+            comp = set()
+            dfs(node,comp)
+            #if you have a group
+            if len(comp) > 0:
+                groups.append(comp)
+        
+        #now make new mapping
+        node_to_color = {}
+        for i in range(len(groups)):
+            nodes = groups[i]
+            for n in nodes:
+                node_to_color[n] = i
+        
+        #check
+        for eq in equations:
+            if eq[1] == '!':
+                a = eq[0]
+                b = eq[-1]
+                if node_to_color[a] == node_to_color[b]:
+                    return False
+        
+        return True
+
+#dfs using coloring
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        '''
+        we can use dfs and color each node along the way
+        we really have an undirecte graph of connected components when we == each variable
+        thenwe just need to check for a contradiction
+        '''
+        adj_list = defaultdict(list)
+        for eq in equations:
+            if eq[1] == '=':
+                a = eq[0]
+                b = eq[-1]
+                adj_list[a].append(b)
+                adj_list[b].append(a)
+        
+        node_to_color = defaultdict()
+        
+        def dfs(node,color):
+            if node not in node_to_color:
+                #mark
+                if node not in node_to_color:
+                    node_to_color[node] = color
+                    for neigh in adj_list[node]:
+                        dfs(neigh,color)
+        
+        #check for all variables between a and b
+        for i in range(26):
+            node = chr(ord('a') + i)
+            color = i
+            if node not in node_to_color:
+                dfs(node,color)
+        
+        #check
+        for eq in equations:
+            if eq[1] == '!':
+                a = eq[0]
+                b = eq[-1]
+                if node_to_color[a] == node_to_color[b]:
+                    return False
+        
+        return True
+
+#union find
+#https://leetcode.com/problems/satisfiability-of-equality-equations/discuss/2625039/LeetCode-The-Hard-Way-Explained-Line-By-Line
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        '''
+        we can use union find
+        the idea is to put all the characters in the same group if they are equal
+        
+        intially each variable points to itself, when we haven == we need to join them using the uion method
+        we build the graph, and then check for a contradiction again
+        '''
+        
+        roots = [i for i in range(26)]
+        
+        def find(x):
+            #keep searching until it points to it selft
+            if x != roots[x]:
+                roots[x] = find(roots[x])
+            
+            return roots[x]
+        
+        def union(x,y):
+            x = find(x)
+            y = find(y)
+            roots[x] = y
+            
+        for eqn in equations:
+            if eqn[1] == '=':
+                x = ord(eqn[0]) - ord('a')
+                y = ord(eqn[-1]) - ord('a')
+                union(x,y)
+        
+        for eqn in equations:
+            if eqn[1] == '!':
+                x = ord(eqn[0]) - ord('a')
+                y = ord(eqn[-1]) - ord('a')
+                if find(x) == find(y):
+                    return False
+        return True
+
+#using only find method
+class Solution:
+    # the idea is to put all characters in the same group if they are equal
+    # in order to do that, we can use Disjoint Set Union (dsu) aka Union Find
+    # for dsu tutorial, please check out https://wingkwong.github.io/leetcode-the-hard-way/tutorials/graph-theory/disjoint-set-union
+    def equationsPossible(self, equations: List[str]) -> bool:
+        # find the root of node x. 
+        # here we are not using parent[x] 
+        # because it may not contain the updated value of the connected component that x belongs to. 
+        # Therefore, we walk the ancestors of the vertex until we reach the root.
+        def find(x):
+            # with path compress
+            if parent[x] == x:
+                return x
+            parent[x] = find(parent[x])
+            return parent[x]
+            # without path compression
+            #return x if parent[x] == x else find(parent[x])
+        # at the beginning, put each character in its own group
+        # so we will have 26 groups with one character each
+        # i.e. 'a' in group 0, 'b' in group 1, ..., 'z' in group 25
+        parent = [i for i in range(26)]
+        for e in equations:
+            if e[1] == '=':
+                # e.g. a == b
+                # then we group them together
+                # how? we use `find` function to find out the parent group of the target character index
+                # then update parent. a & b would be in group 1 (i.e. a merged into the group where b belongs to)
+                # or you can also do `parent[find(ord(e[3]) - ord('a'))] = find(ord(e[0]) - ord('a'))`
+                # i.e. b merged into the group where a belongs to
+                parent[find(ord(e[0]) - ord('a'))] = find(ord(e[3]) - ord('a'))
+        # handle != case
+        for e in equations:
+            # if two characters are not equal
+            # then which means their parent must not be equal
+            if e[1] == '!' and find(ord(e[0]) - ord('a')) == find(ord(e[3]) - ord('a')):
+                return False
+        return True
