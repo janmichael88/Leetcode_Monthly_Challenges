2e7a906fe24aab6ad69c569cb82fe7da4015d3f5
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2022-10-14 19:24:23 -0700

    889. Construct Binary Tree from Preorder and Postorder Traversal

--- Oct_22.py
+++ Oct_22.py
@@ -1002,6 +1002,59 @@ class Solution:
 		
 		return build(0,len(preorder)-1,0,len(postorder)-1)
 
+#https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/discuss/748216/Python3-Solution-with-a-Detailed-Explanation-Construct-Binary-Tree-from
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
+        # read this: https://www.techiedelight.com/construct-full-binary-tree-from-preorder-postorder-sequence/
+        def helper(pre,post):
+            print('pre is: ', pre, 'post is: ', post)
+            if not pre:
+                return None
+        
+            if len(pre)==1:
+                return TreeNode(post.pop())
+        
+        
+            node=TreeNode(post.pop()) #3
+            ind=pre.index(post[-1]) #4
+        
+            node.right=helper(pre[ind:],post) #1
+            node.left=helper(pre[1:ind],post) #2
+            return node
+    
+        return helper(pre,post)
+
+#hashmap for fast lookup
+class Solution:
+	def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
+    
+    idx={}
+    for i,j in enumerate(pre):
+        idx[j]=i
+    def helper(start,end,post):
+        
+        if end==start:
+            return None
+    
+        if end-start==1:
+            return TreeNode(post.pop())
+    
+        
+        node=TreeNode(post.pop()) #3
+        ind=idx[post[-1]] #4
+        
+        node.right=helper(ind,end,post) #1
+        node.left=helper(start+1,ind,post) #2
+        return node
+
+    return helper(0,len(pre),post)
+
 ###############################################
 # 2095. Delete the Middle Node of a Linked List
 # 14OCT22
@@ -1098,3 +1151,8 @@ class Solution:
             ans.append(give_them >= curr_max)
         
         return ans
+
+####################################
+# 149. Max Points on a Line
+# 14OCT22
+####################################
