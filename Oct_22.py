7e79e495b03f7c2b5f6ec9507c09f7b16cf0963b
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2022-10-27 11:16:55 -0700

    523. Continuous Subarray Sum

--- Oct_22.py
+++ Oct_22.py
@@ -2463,6 +2463,16 @@ class Solution():
         Idea: if sum(nums[i:j]) % k == 0 for some i < j, then sum(nums[:j]) % k == sum(nums[:i]) % k. So we just need to use a dictionary to keep track of sum(nums[:i]) % k and the corresponding index i. Once some later sum(nums[:i']) % k == sum(nums[:i]) % k and i' - i > 1, we return True.
 
 		Time complexity: O(n), space complexity: O(min(k, n)) if k != 0, else O(n).
+
+		This helped me visualize the ans. In the example:
+		[23,2,6,4,7] k =12
+
+		(23)%12 = 11
+		(23+2)%12=1
+		(23+2+6)%12 = 7
+		(23+2+6+4)%12=11 -> seen before, so return true because:
+
+		(23+2+6+4) - (23) = 12 -> 12 % 12 = 0
         '''
         dic = {0:-1}
         summ = 0
@@ -2474,3 +2484,109 @@ class Solution():
                 if i - dic[summ] >= 2:
                     return True
         return False
+
+#############################
+# 273. Integer to English Words
+# 26OCT22
+#############################
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        '''
+        we can divide the number into sets of three and solve
+        for example: 
+        1234567890
+        1.234.567.890
+        1 billion 234 million 567 thousand 890
+        ONE billion 2 hundred 34 5 hundred 67...
+        
+        we keep dividing
+        
+        problem is really jusy about edge cases and how to handle them in the right order
+        
+        '''
+        def one(num):
+            switcher = {
+                1: 'One',
+                2: 'Two',
+                3: 'Three',
+                4: 'Four',
+                5: 'Five',
+                6: 'Six',
+                7: 'Seven',
+                8: 'Eight',
+                9: 'Nine'
+            }
+            return switcher.get(num)
+
+        def two_less_20(num):
+            switcher = {
+                10: 'Ten',
+                11: 'Eleven',
+                12: 'Twelve',
+                13: 'Thirteen',
+                14: 'Fourteen',
+                15: 'Fifteen',
+                16: 'Sixteen',
+                17: 'Seventeen',
+                18: 'Eighteen',
+                19: 'Nineteen'
+            }
+            return switcher.get(num)
+        
+        def ten(num):
+            switcher = {
+                2: 'Twenty',
+                3: 'Thirty',
+                4: 'Forty',
+                5: 'Fifty',
+                6: 'Sixty',
+                7: 'Seventy',
+                8: 'Eighty',
+                9: 'Ninety'
+            }
+            return switcher.get(num)
+        
+
+        def two(num):
+            if not num:
+                return ''
+            elif num < 10:
+                return one(num)
+            elif num < 20:
+                return two_less_20(num)
+            else:
+                tenner = num // 10
+                rest = num - tenner * 10
+                return ten(tenner) + ' ' + one(rest) if rest else ten(tenner)
+        
+        def three(num):
+            hundred = num // 100
+            rest = num - hundred * 100
+            if hundred and rest:
+                return one(hundred) + ' Hundred ' + two(rest) 
+            elif not hundred and rest: 
+                return two(rest)
+            elif hundred and not rest:
+                return one(hundred) + ' Hundred'
+        
+        billion = num // 1000000000
+        million = (num - billion * 1000000000) // 1000000
+        thousand = (num - billion * 1000000000 - million * 1000000) // 1000
+        rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000
+        
+        if not num:
+            return 'Zero'
+        
+        result = ''
+        if billion:        
+            result = three(billion) + ' Billion'
+        if million:
+            result += ' ' if result else ''    
+            result += three(million) + ' Million'
+        if thousand:
+            result += ' ' if result else ''
+            result += three(thousand) + ' Thousand'
+        if rest:
+            result += ' ' if result else ''
+            result += three(rest)
+        return result
