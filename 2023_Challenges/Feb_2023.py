e817e0783b0e291e8bc9853402787b9b3b1a0bea
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2023-02-03 09:54:42 -0800

    1254. Number of Closed Islands

--- 2023_Challenges/Feb_2023.py
+++ 2023_Challenges/Feb_2023.py
@@ -137,4 +137,208 @@ class Solution:
 
         k = gcd(len(str1),len(str2))
         return str1[:k]
+
+################################################
+# 953. Verifying an Alien Dictionary (REVISTED)
+# 02FEB23
+################################################
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        '''
+        just compare each ith word to the i + 1 word
+        '''
+        ordering = {ch:i for i,ch in enumerate(order)}
+        N = len(words)
         
+        
+        for i in range(1,N):
+            first_word = words[i-1]
+            second_word = words[i]
+            #pointers into the words
+            j = 0
+            k = 0
+            while j < len(first_word) and k < len(second_word):
+                #get chars
+                first_char = first_word[j]
+                second_char = second_word[k]
+                #less than, first occurent leave loopd
+                if ordering[first_char] < ordering[second_char]:
+                    break
+                #violation
+                elif ordering[first_char] > ordering[second_char]:
+                    return False
+                else:
+                    j += 1
+                    k += 1
+            #in the case we have fall through after breaking
+            #this fires only after a break in the before while loop
+            else:
+                #size
+                if len(first_word) > len(second_word):
+                    return False
+
+        return True
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        '''
+        also could have used comparator in the sorting method
+        '''
+        ordering = {ch:i for i,ch in enumerate(order)}
+        
+        #function
+        order_words = lambda word : [ordering[ch] for ch in word ]
+        
+        #sort
+        sorted_words = sorted(words, key =  order_words)
+        return sorted_words == words
+
+#####################################
+# 1254. Number of Closed Islands
+# 02FEB23
+#####################################
+#bleaghhhh
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        '''
+        0's are land, and 1's are water
+        count number of closed islans
+        
+        i could get all the islands, then just check that each island is closed
+        '''
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        islands = []
+        dirrs = [[1,0],[-1,0],[0,1],[0,-1]]
+        seen = set()
+        
+        def dfs(x,y,curr_island,seen):
+            #add to currnet island
+            curr_island.append([x,y])
+            #add to seen
+            seen.add((x,y))
+            for dx,dy in dirrs:
+                neigh_x = x + dx
+                neigh_y = y + dy
+                #bounds check
+                if 0 <= neigh_x < rows and 0 <= neigh_y < cols:
+                    #not seen and is land
+                    if (neigh_x,neigh_y) not in seen and grid[neigh_x][neigh_y] == 0:
+                        dfs(neigh_x,neigh_y,curr_island,seen)
+                        
+                        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0 and (i,j) not in seen:
+                    curr_island = []
+                    dfs(i,j,curr_island,seen)
+                    islands.append(curr_island)
+        
+        closed_islands = 0
+        corner_cells = [[0,0],[rows-1,0],[0,cols-1],[rows-1,cols-1]]
+        for isl in islands:
+            #exclude corner cells
+            for cell in isl:
+                if cell in corner_cells:
+                    break
+            else:
+                closed_islands += 1
+        
+        return closed_islands
+
+
+#flood fill the the connected 0s to an edge with 1ones
+#then dfs and count connected components
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        '''
+        flood fill edge containing 0s with ones, then just dfs and count connected components
+        we don'y neet a seen set, we can just mutate the grid in place
+        careful with xy, and ij
+        '''
+        rows = len(grid)
+        cols = len(grid[0])
+        dirrs = [[1,0],[-1,0],[0,1],[0,-1]]
+        
+        
+        def dfs(x,y):
+            #fill with 1
+            grid[x][y] = 1
+            for dx,dy in dirrs:
+                neigh_x = x + dx
+                neigh_y = y + dy
+                if 0 <= neigh_x < rows and 0 <= neigh_y < cols:
+                    if grid[neigh_x][neigh_y] == 0:
+                        dfs(neigh_x,neigh_y)
+                        
+        #first fill in edges 
+        for i in range(rows):
+            for j in range(cols):
+                if i == 0 or j == 0 or i == rows -1 or j == cols - 1:
+                    if grid[i][j] == 0:
+                        dfs(i,j)
+        
+        
+        ans = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    dfs(i,j)
+                    ans += 1
+        
+        return ans
+
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        '''
+        if i hit aborder this is not a captured island
+        '''
+        m, n = len(grid), len(grid[0])
+        res = 0
+        
+        def dfs(x, y):
+            if x in (0, m-1) or y in (0, n-1):
+                self.isIsland = False 
+            for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                if 0 <= i < m and 0 <= j < n and grid[i][j] == 0:
+                    grid[i][j] = 1 
+                    dfs(i, j)
+                    
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    self.isIsland = True
+                    dfs(i, j)
+                    res += self.isIsland
+                    
+        return res 
+
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        '''
+        if i hit aborder this is not a captured island
+        '''
+        rows, cols = len(grid), len(grid[0])
+        captured_islands = 0
+        
+        def dfs(x, y):
+            #touches edge
+            if x in (0, rows-1) or y in (0, cols-1):
+                #just doing it a different way, non self variable
+                #touches an edge means it is not a captured island
+                nonlocal is_Captured
+                is_Captured = False 
+            for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+                if 0 <= i < rows and 0 <= j < cols and grid[i][j] == 0:
+                    grid[i][j] = 1 
+                    dfs(i, j)
+                    
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    is_Captured = True
+                    dfs(i, j)
+                    captured_islands += is_Captured
+                    
+        return captured_islands 
