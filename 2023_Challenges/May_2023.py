590dfc3be99973f2355c25f9a55afd88f394cb73
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2023-05-11 23:14:28 -0700

    311. Sparse Matrix Multiplication

--- 2023_Challenges/May_2023.py
+++ 2023_Challenges/May_2023.py
@@ -981,7 +981,89 @@ class Solution:
         return ans
     
 #sparse row and sparse col
+class SparseMatrix:
+    def __init__(self, matrix: List[List[int]], col_wise: bool):
+        self.values, self.row_index, self.col_index = self.compress_matrix(matrix, col_wise)
 
+    def compress_matrix(self, matrix: List[List[int]], col_wise: bool):
+        return self.compress_col_wise(matrix) if col_wise else self.compress_row_wise(matrix)
+
+    # Compressed Sparse Row
+    def compress_row_wise(self, matrix: List[List[int]]):
+        values = []
+        row_index = [0]
+        col_index = []
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col]:
+                    values.append(matrix[row][col])
+                    col_index.append(col)
+            row_index.append(len(values))
+
+        return values, row_index, col_index
+
+    # Compressed Sparse Column
+    def compress_col_wise(self, matrix: List[List[int]]):
+        values = []
+        row_index = []
+        col_index = [0]
+
+        for col in range(len(matrix[0])):
+            for row in range(len(matrix)):
+                if matrix[row][col]:
+                    values.append(matrix[row][col])
+                    row_index.append(row)
+            col_index.append(len(values))
+
+        return values, row_index, col_index
+    
+
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        '''
+        yale format, compressed sparse row (CSR), compressed sparse col (CSC)
+        three arrays
+            values: array containing all non zero eleements
+            colIndex: array containg column index of all non zero elements
+            rowIndex: array storing valuesf of startindex of each rows
+        
+        len(values) == len(colIndex) === number of nonzero elements in matrix
+        rowIndex = num_rows + 1, where 
+            rowIndex[i] to rowIndex[i+1] (non inclusive) gives us the range index where the ith row element of matrix ar stored
+            
+        we compress mat1 as CSR, and mat2 as CSC, then we can easily find an element in row i of mat1 and in col j of mat2
+        we can use the two pointer tricker and we keep advnacing until row i == cold j
+        '''
+        A = SparseMatrix(mat1,False)
+        B = SparseMatrix(mat2,True)
+        
+        ans = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+
+        for row in range(len(ans)):
+            for col in range(len(ans[0])):
+
+                # Row element range indices
+                mat1_row_start = A.row_index[row]
+                mat1_row_end = A.row_index[row + 1]
+
+                # Column element range indices
+                mat2_col_start = B.col_index[col]
+                mat2_col_end = B.col_index[col + 1]
+
+                # Iterate over both row and column.
+                while mat1_row_start < mat1_row_end and mat2_col_start < mat2_col_end:
+                    if A.col_index[mat1_row_start] < B.row_index[mat2_col_start]:
+                        mat1_row_start += 1
+                    elif A.col_index[mat1_row_start] > B.row_index[mat2_col_start]:
+                        mat2_col_start += 1
+                    # Row index and col index are same so we can multiply these elements.
+                    else:
+                        ans[row][col] += A.values[mat1_row_start] * B.values[mat2_col_start]
+                        mat1_row_start += 1
+                        mat2_col_start += 1
+    
+        return ans
 
 #############################################
 # 59. Spiral Matrix II
