e6003e2808e2b6cffee0201cd6d28c2120cfc10a
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2023-12-08 01:04:35 -0800

    1973. Count Nodes Equal to Sum of Descendants

--- 2023_Challenges/Dec_2023.py
+++ 2023_Challenges/Dec_2023.py
@@ -683,3 +683,33 @@ class Solution:
             
         
         return int("".join(num))
+    
+################################################
+# 1973. Count Nodes Equal to Sum of Descendants
+# 08DEC23
+################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def equalToDescendants(self, root: Optional[TreeNode]) -> int:
+        '''
+        carry sums and chekc if == to node.val
+        '''
+        count = [0]
+        
+        def dp(node):
+            if not node:
+                return 0
+            left = dp(node.left)
+            right = dp(node.right)
+            if left + right == node.val:
+                count[0] += 1
+            
+            return left + right + node.val
+        
+        _ = dp(root)
+        return count[0]
