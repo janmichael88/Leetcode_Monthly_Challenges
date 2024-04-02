9b7b70f0435762e6951a0e9f33e13d6cc1b3bf54
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2024-04-02 09:42:04 -0700

    885. Spiral Matrix III

--- 2024_Challenges/Apr_2024.py
+++ 2024_Challenges/Apr_2024.py
@@ -61,3 +61,108 @@ class Solution:
         
         return drink + numBottles
 
+###############################################################
+# 1779. Find Nearest Point That Has the Same X or Y Coordinate
+# 02APR24
+###############################################################
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        '''
+        just check all points only if x or y line up
+        '''
+        min_dist = float('inf')
+        min_index = float('inf')
+        
+        for i_i,point in enumerate(points):
+            i,j = point
+            if i == x or j == y:
+                curr_dist = abs(i-x) + abs(j-y)
+                if curr_dist < min_dist:
+                    min_dist = curr_dist
+                    min_index = i_i
+        
+        if min_index == float('inf'):
+            return -1
+        return min_index
+    
+################################
+# 885. Spiral Matrix III
+# 02APR24
+################################
+#close one
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        '''
+        walk the spiral and keep going until we touch all visit all cells
+        spiral is
+        (start) , (right,one), (down,one),(left,two), (up,two),(right,three)
+        so its R,D,L,U repeating, increase dist by 1 every time
+        '''
+        dirrs = [
+            [1,0],[0,1],[-1,0],[0,-1]
+        ]
+        
+        cells = rows*cols
+        visited = []
+        dirr_ptr = 0
+        curr_step = -1
+        step_size = 1
+        
+        #start
+        visited.append([rStart,cStart])
+        cells -= 1
+        
+        while cells > 0:
+            if curr_step % 2 == 0:
+                step_size += 1
+            d_y,d_x = dirrs[dirr_ptr]
+            #move to next cell using dirr_ptr and step_size
+            rStart = rStart + d_x*step_size
+            cStart = cStart + d_y*step_size
+            print(rStart,cStart)
+            #if in bounds, add it
+            if 0 <= rStart < rows and 0 <= cStart < cols:
+                visited.append([rStart,cStart])
+                cells -= 1
+            
+            dirr_ptr = (dirr_ptr + 1) % 4
+            curr_step += 1
+        
+        return visited
+
+#right idea, just break it into 4 walks
+#incremnt step size by 2 each time
+class Solution(object):
+    
+    def spiralMatrixIII(self, R, C, r0, c0):
+        ret = [(r0, c0)] 
+        is_valid = lambda row, col: row >= 0 and row < R and col >= 0 and col < C 
+        
+        steps = 1 
+        r, c = r0, c0 
+        while len(ret) < R * C: 
+            # Go east 1
+            for step in range(steps):
+                r, c = r, c + 1 
+                if is_valid(r, c): ret.append((r, c))
+                    
+            # Go down 1 
+            for step in range(steps):
+                r, c = r + 1, c 
+                if is_valid(r, c): ret.append((r, c))
+                    
+            steps += 1
+                    
+            # Go west 2 
+            for step in range(steps):
+                r, c = r, c - 1
+                if is_valid(r, c): ret.append((r, c))           
+            
+            # Go north 2 
+            for step in range(steps):
+                r, c = r - 1, c 
+                if is_valid(r, c): ret.append((r, c))           
+                    
+            steps += 1
+            
+        return ret 
