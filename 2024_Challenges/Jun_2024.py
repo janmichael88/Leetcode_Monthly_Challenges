1fb0a9c83944ef8ba9e03b3c0a4aec258da2746a
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2024-06-04 21:31:37 -0700

    1668. Maximum Repeating Substring

--- 2024_Challenges/Jun_2024.py
+++ 2024_Challenges/Jun_2024.py
@@ -75,3 +75,166 @@ class Solution:
                 i += 1
         
         return len(t) - i
+
+#################################
+# 409. Longest Palindrome (REVISITED)
+# 04JUN24
+#####################################
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        for even length palindrome, letters must exsist in pairs
+        for odd lenght, all letters except the center must exsist in pairs
+        heap with pairs and take by two, until top is 1
+        '''
+        counts = Counter(s)
+        max_heap = [-count for _,count in counts.items()]
+        heapq.heapify(max_heap)
+        
+        ans = 0
+        while max_heap:
+            if max_heap[0] == -1:
+                break
+            curr_count = heapq.heappop(max_heap)
+            ans += 2
+            curr_count += 2
+            if curr_count < 0:
+                heapq.heappush(max_heap, curr_count)
+        
+        if max_heap and max_heap[0] == -1:
+            ans += 1
+        return ans
+    
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        sort and use pairs, if i have a left over one add it
+        '''
+        counts = Counter(s)
+        counts = [count for _,count in counts.items()]
+        counts.sort(reverse = True)
+        ans = 0
+        last_one = False
+        
+        for num in counts:
+            if num == 1:
+                last_one = True
+                break
+            else:
+                if num % 2 == 0:
+                    ans += num
+                else:
+                    ans += num - 1
+                    last_one = True
+        
+        if last_one:
+            ans += 1
+        
+        return ans
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        you dont need to sort
+        '''
+        counts = Counter(s)
+        counts = [count for _,count in counts.items()]
+        ans = 0
+        last_one = False
+        
+        for num in counts:
+            if num % 2 == 0:
+                ans += num
+            else:
+                ans += num - 1
+                last_one = True
+        
+        if last_one:
+            ans += 1
+        
+        return ans
+                 
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        '''
+        we can count can build on the fly
+        odd counts of characters will have 1 unused letter to pair, unless its 1, then its the center
+        we use this intution to keep track of the charactes that have an odd frequency
+        then just subtract from len(s)
+        '''
+        counts = defaultdict(int)
+        leftover_odds = 0
+        
+        for ch in s:
+            counts[ch] += 1
+            if counts[ch] % 2 == 1:
+                leftover_odds += 1
+            else:
+                leftover_odds -= 1
+        
+        if leftover_odds > 0:
+            return len(s) - leftover_odds + 1
+        return len(s)
+            
+###########################################
+# 1668. Maximum Repeating Substring
+# 04JUN24
+###########################################
+#doesn't quite work
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        '''
+        just slide and count
+        crap its k-repeating
+        its just count streak but with word
+        '''
+        L = len(sequence)
+        M = len(word)
+        
+        if word not in sequence:
+            return 0
+        max_streak = 1
+        curr_streak = 0
+        i = 0
+        while i < L-M+1:
+            if sequence[i:i+M] == word:
+                curr_streak += 1
+                i += M
+            else:
+                max_streak = max(max_streak,curr_streak)
+                curr_streak = 0
+                i += 1
+        
+        return max(max_streak,curr_streak)
+    
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        '''
+        brute force would be to try all k
+        '''
+        L = len(sequence)
+        M = len(word)
+        
+        for k in range(L-M+1,-1,-1):
+            kth_repeating = word*k
+            if kth_repeating in sequence:
+                return k
+        
+        return 0
+    
+#binary search workable
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        if word not in sequence:
+            return 0
+
+        left = 1
+        right = len(sequence) - len(word) + 1
+        while left <= right:
+            mid = (left + right) // 2
+            if word * mid in sequence:
+                left = mid + 1
+            else:
+                right = mid - 1 
+                
+        return left - 1
