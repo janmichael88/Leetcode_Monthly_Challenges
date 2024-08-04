2b03c409ed7431070ab08897e60f657c176ebfdb
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2024-08-04 12:09:53 -0400

    1508. Range Sum of Sorted Subarray Sums

--- 2024_Challenges/Aug_2024.py
+++ 2024_Challenges/Aug_2024.py
@@ -90,3 +90,91 @@ class Solution:
                 left += 1
 
         return ans
+    
+###########################################
+# 1062. Longest Repeating Substring
+# 02JUL24
+############################################
+#cheeze
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+        '''
+        generate all possible substrings
+        '''
+        seen = set()
+        N = len(s)
+        ans = 0
+        for i in range(N):
+            for j in range(i+1,N+1):
+                substring = s[i:j]
+                if substring in seen:
+                    ans = max(ans,len(substring))
+                else:
+                    seen.add(substring)
+        
+        return ans
+    
+#using rolling hash
+#doesnt quite work collisiosn???
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+        '''
+        need to use rolling hash instead of getting subsstring
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+        '''
+        print(len(s))
+        seen = set()
+        N = len(s)
+        ans = 0
+        mod = 10**9 + 7
+        base = 31
+        for i in range(N):
+            curr_hash = 0
+            p_pow = 1
+            for j in range(i,N):
+                dig = ord(s[j]) - ord('a')
+                curr_hash = (curr_hash + (dig + 1)*p_pow) % mod
+                p_pow = (p_pow*base) % mod
+                if curr_hash in seen:
+                    ans = max(ans,j-i+1)
+                else:
+                    seen.add(curr_hash)
+        print(len(seen))
+        return ans
+
+
+class Solution:
+    def longestRepeatingSubstring(self, s: str) -> int:
+        
+        def check_length(length):
+            seen = set()
+            base = 26
+            mod = 2**31 - 1
+            current_hash = 0
+            base_power = 1
+            for i in range(length):
+                current_hash = (current_hash * base + ord(s[i])) % mod
+                base_power = (base_power * base) % mod
+            
+            seen.add(current_hash)
+            
+            for start in range(1, len(s) - length + 1):
+                current_hash = (current_hash * base - ord(s[start - 1]) * base_power + ord(s[start + length - 1])) % mod
+                if current_hash in seen:
+                    return True
+                seen.add(current_hash)
+            
+            return False
+        
+        left, right = 1, len(s)
+        ans = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if check_length(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return ans
