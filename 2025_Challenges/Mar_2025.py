492699e7873a9cfc16ccc5bc9a12c3e17222c484
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2025-03-05 08:13:53 -0800

    1780. Check if Number is a Sum of Powers of Three

--- 2025_Challenges/Mar_2025.py
+++ 2025_Challenges/Mar_2025.py
@@ -109,3 +109,204 @@ class Solution:
                 remaining.append(num)
         
         return less_than + equal_to + remaining
+    
+#two pass pointers
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        '''
+        use pointers and keep track of where they should belong
+        count up less and equal too
+        greater elements start at less + equal
+        '''
+        less_than = 0
+        equal_to = 0
+        for num in nums:
+            if num < pivot:
+                less_than += 1
+            elif num == pivot:
+                equal_to += 1
+        
+        ans = [0]*len(nums)
+        less_ptr = 0
+        #equal starts at less than count
+        equal_ptr = less_than
+        greater_ptr = less_than + equal_to
+
+        for num in nums:
+            if num < pivot:
+                ans[less_ptr] = num
+                less_ptr += 1
+            elif num > pivot:
+                ans[greater_ptr] = num
+                greater_ptr += 1
+            else:
+                ans[equal_ptr] = num
+                equal_ptr += 1
+        
+        return ans
+    
+########################################
+# 2747. Count Zero Request Servers
+# 04MAR25
+#########################################
+#wtf???
+class Solution:
+    def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
+        '''
+        sort queries and logs in increasing order,
+        then sliding window to find answers
+        i need to maintain the original query order
+        '''
+        logs.sort(key = lambda x : x[1])
+        queries = [(i,q) for i,q in enumerate(queries)]
+        queries.sort(key = lambda x: x[1])
+        ans = [0]*len(queries)
+        left = 0
+        right = 0
+        servers = Counter()
+        used = 0
+        for i,q in queries:
+            while right < len(logs) and logs[right][1] <= q:
+                #add to window
+                servers[logs[right][0]] += 1
+                used += servers[logs[right][0]] == 1
+                right += 1
+            
+            #shrink
+            while left < right and logs[left][1] < q - x:
+                servers[logs[left][0]] -= 1
+                used -= servers[logs[left][0]] == 0
+                left += 1
+            
+            ans[i] = n - used
+        
+        return ans
+
+####################################################
+# 1780. Check if Number is a Sum of Powers of Three
+# 04MAR25
+####################################################
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        how many powers of three are there?
+        there are log_3(n) powers of three
+        of these powers of three try and see if we can make a n
+        must be distinc
+        '''
+        powers_of_three = []
+        x = -1
+        while 3**x <= n:
+            x += 1
+            powers_of_three.append(3**x)
+        
+        def rec(n,i):
+            if i >= len(powers_of_three):
+                return False
+            if n < 0:
+                return False
+            if n == 0:
+                return True
+            for j in range(i,len(powers_of_three)):
+                power = powers_of_three[j]
+                if rec(n-power,j+1):
+                    return True
+            return False
+        
+        return rec(n,0)
+    
+#this TLE's though
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        how many powers of three are there?
+        there are log_3(n) powers of three
+        of these powers of three try and see if we can make a n
+        must be distinct,
+        im not satisfied that the test cases are strong enough
+
+        also could have done backtracking
+        '''
+        powers_of_three = []
+        x = -1
+        while 3**x <= n:
+            x += 1
+            powers_of_three.append(3**x)
+        
+        used = set()
+        
+        def rec(n,used):
+            if n < 0:
+                return False
+            if n == 0:
+                return True
+            for power in powers_of_three:
+                if power not in used:
+                    used.add(power)
+                    if rec(n-power,used):
+                        return True
+                    used.remove(power)
+            return False
+        
+        return rec(n,used)
+
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        we can treat as knapsack and keep track of power and n - 3^(some_power)
+        i don't think there are repated subproblems for this one though
+        '''
+        memo = {}
+        def rec(power,n):
+            if n == 0:
+                return True
+            if 3**power > n:
+                return False
+            if (power,n) in memo:
+                return memo[(power,n)]
+            take = rec(power+1,n-3**power)
+            no_take = rec(power+1,n)
+            ans = take or no_take
+            memo[(power,n)] = ans
+            return ans
+        
+        return rec(0,n)
+    
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        assume we have all powers of three as a,b,c...wtv char as needed
+        is n is a sum of all these then we can write
+        n = a + b + c + d + e ....
+        keep using the largest power
+        '''
+        largest_power = 0
+        while n >= 3**largest_power:
+            largest_power += 1
+        
+        while n > 0:
+            #try using power
+            if n >= 3**largest_power:
+                n -= 3**largest_power
+            #cannot use same power twise
+            if n >= 3**largest_power:
+                return False
+            largest_power -= 1
+        
+        return True
+    
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        '''
+        keep diving by 3 and check mod 3
+        if the numbers a,b,c,d,e are all powers of three
+        then a + b + c + d + e is divible by 3
+        '''
+        while n > 0:
+            #check if power should be used only once
+            print(n % 3)
+            if n % 3 == 2:
+                return False
+            n = n // 3
+        
+        return True
