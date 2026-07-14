c00d08d8e5d4c90b3ae5409929cd979ad009539a
janmichael88 <55464895+janmichael88@users.noreply.github.com>
2026-07-14 14:20:30 -0700

    14JUL26

--- 2026_Challenges/Jul_2026.py
+++ 2026_Challenges/Jul_2026.py
@@ -313,3 +313,341 @@ class Solution:
         
         return [score,ways]
 
+####################################################
+# Concatenate Non-Zero Digits and Multiply by Sum I
+# 06JUL26
+####################################################
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        '''
+        pull the digits
+        '''
+        digits = [int(ch) for ch in  str(n)]
+        x = 0
+        sum_ = 0
+        for ch in digits:
+            if ch != 0:
+                x = x*10 + ch
+                sum_ += ch
+        
+        return x*sum_
+    
+############################################################
+# 3756. Concatenate Non-Zero Digits and Multiply by Sum II
+# 08JUL26
+##############################################################
+#TLE
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        '''
+        use prefix sum for x of digits
+        and prefix concantention but its a little different
+            this is a little different, we cant do it like normal pref_sum because the zeros break it
+            similar to hashing, but the standard trick is to remove the lft pary by diving by the appropriate power of 10
+        
+        pref_concat stors concat up to i of all non zero digits
+        pref_count number of non-zero digits before i
+        '''
+        pref_sum = [0]
+        for ch in s:
+            pref_sum.append(int(ch) + pref_sum[-1])
+
+        pref_concat = [0]
+        pref_cnt = [0]
+
+        for ch in s:
+            d = int(ch)
+            if d:
+                pref_concat.append(pref_concat[-1] * 10 + d)
+                pref_cnt.append(pref_cnt[-1] + 1)
+            else:
+                pref_concat.append(pref_concat[-1])
+                pref_cnt.append(pref_cnt[-1])
+
+        
+        ans = []
+        mod = 10**9 + 7
+        for l,r in queries:
+            x = pref_sum[r+1] - pref_sum[l]
+            k = pref_cnt[r+1] % mod - pref_cnt[l] % mod
+            concat = (pref_concat[r+1] % (10**k)) % mod
+            ans.append(x*concat % mod)
+
+        return ans
+    
+#finally
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        '''
+        use prefix sum for x of digits
+        and prefix concantention but its a little different
+            this is a little different, we cant do it like normal pref_sum because the zeros break it
+            similar to hashing, but the standard trick is to remove the lft pary by diving by the appropriate power of 10
+        
+        pref_concat stors concat up to i of all non zero digits
+        pref_count number of non-zero digits before i
+        recall rolling  hash
+        hash(l, r) = pref[r] - pref[l] * base^(r-l)
+
+        left_part = 123
+        answer    = 456
+        k = 3
+
+        pref_concat[r+1] = 123456
+
+        answer = pref_concat[r+1] - left_part * 10^k
+        answer = (pref_concat[r+1] - pref_concat[l] * pow10[k]) % MOD
+        '''
+        mod = 10**9 + 7
+
+        pref_sum = [0]
+        pref_concat = [0]
+        pref_cnt = [0]
+
+        for ch in s:
+            d = int(ch)
+            pref_sum.append(pref_sum[-1] + d)
+
+            if d:
+                pref_concat.append((pref_concat[-1] * 10 + d) % mod)
+                pref_cnt.append(pref_cnt[-1] + 1)
+            else:
+                pref_concat.append(pref_concat[-1])
+                pref_cnt.append(pref_cnt[-1])
+
+        pow10 = [1] * (pref_cnt[-1] + 1)
+        for i in range(1, len(pow10)):
+            pow10[i] = (pow10[i - 1] * 10) % mod
+
+
+        ans = []
+        for l, r in queries:
+            x = pref_sum[r + 1] - pref_sum[l]
+            k = pref_cnt[r + 1] - pref_cnt[l]
+            concat = (pref_concat[r + 1] - pref_concat[l] * pow10[k]) % mod
+            ans.append(x * concat % mod)
+
+        return ans
+
+##################################################
+# 3534. Path Existence Queries in a Graph II
+# 11JUL26
+###################################################
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
+        '''
+        lets comb through this problem so we can finally get over binary lifiting
+        first thing to note is that the graph is implicit, so we have numbers ordered like 
+        1 --3 --5 --8 --10 --12, with maxDiff = 3
+        the firstt comp is 1,3,5 then 8,10,12
+
+        so if we have nums[i] <= nums[j] <= nums[k] amd nums[k] - nums[i] > maxDiff, i cannot reach anything after k
+        but we need the minimum distatance, so we need to track jumps, but we can't just loop through all the jumps for each query
+        that would take too long, O(n), 
+
+        so for each node, we need to record the furthest distance, going back to our first exaplem wed have something like
+        1 -> {3,5}
+        3 -> {5,6}
+        so its kinda like jump game, and we can ask
+        Starting at index i, repeatedly replace, where we have i = next[i], how many jumps is that
+
+        notes on binary lifiting, at its cores its like jumping by powers of two, its very standard, either know it or dont
+        it creates a sprase table, and use the sparse table to answer queries 
+        suppose we have a linked list like : 0 -> 3 -> 5 -> 8 -> 10 -> None
+        and we want to ask, where are you after 13 jumps from node 0
+        the dumb way is just do the jumps itertiavely from 0, and on, but that requires O(jump) time ~ O(n)
+
+        a better way would be to compure where i am after 2 jumps, 4, jumps, 8.... all the way,
+        jump1[x] = next node
+        jump2[x] = next[next[x]]
+        jump4[x] = jump2[jump2[x]]
+        jump8[x] = jump4[jump4[x]]
+
+        this becomes sparse table
+                        1 jump   2 jumps   4 jumps   8 jumps
+
+            0            3         5         10       None
+            3            5         8        None      None
+            5            8        10        None      None
+            8           10       None       None      None
+
+        notice each column is twice the previous one
+
+        so now if i ask, for 13 jumps
+        13 in binary is 13 = 8 + 4 + 1
+        get jumps for each from each column, thats only 3 ops, we've reduced to O(jump) to O(log(jump))
+
+        the general formula
+        up[k][v] menas where are you after 2**k jumps
+        so we have, up[0][v] = next[v]
+        ans we have, up[1][v] = up[0][ up[0][v] ] 
+        and we have  up[2][v] = up[1][ up[1][v] ]
+        generally we have up[k][v] = up[k-1][ up[k-1][v] ], which is bottom up dp
+
+        then answering a query becomes like this,
+        wherre am i after 19 jumps? first convert to binary 10011, which is 16 + 2 + 1
+        and we get this:
+                node = start
+
+        for k in reversed(range(LOG)):
+            if (steps >> k) & 1: #check bit is set
+                node = up[k][node]
+        '''
+        # --------------------------------------------------
+        # Step 1. Sort nodes by value
+        # --------------------------------------------------
+        arr = sorted((nums[i], i) for i in range(n))
+
+        # pos[original node] = position in sorted order
+        pos = [0] * n
+        values = [0] * n
+
+        for i, (val, node) in enumerate(arr):
+            pos[node] = i
+            values[i] = val
+
+        # --------------------------------------------------
+        # Step 2. Compute next[i]
+        # next[i] = furthest index reachable in ONE edge
+        # --------------------------------------------------
+        next_ = [0] * n
+
+        j = 0
+        for i in range(n):
+            if j < i:
+                j = i
+
+            while j + 1 < n and values[j + 1] - values[i] <= maxDiff:
+                j += 1
+
+            next_[i] = j
+
+        # --------------------------------------------------
+        # Step 3. Connected components
+        # Every gap > maxDiff starts a new component.
+        # --------------------------------------------------
+        comp = [0] * n
+
+        cid = 0
+        comp[0] = 0
+
+        for i in range(1, n):
+            if values[i] - values[i - 1] > maxDiff:
+                cid += 1
+            comp[i] = cid
+
+        # --------------------------------------------------
+        # Step 4. Binary lifting
+        # up[k][i] = position after 2^k greedy jumps
+        # --------------------------------------------------
+        LOG = n.bit_length()
+
+        up = [[0] * n for _ in range(LOG)]
+        up[0] = next_[:]
+
+        for k in range(1, LOG):
+            for i in range(n):
+                up[k][i] = up[k - 1][up[k - 1][i]]
+
+        # --------------------------------------------------
+        # Step 5. Answer queries
+        # --------------------------------------------------
+        ans = []
+
+        for u, v in queries:
+
+            a = pos[u]
+            b = pos[v]
+
+            if a > b:
+                a, b = b, a
+
+            # Different connected components
+            if comp[a] != comp[b]:
+                ans.append(-1)
+                continue
+
+            if a == b:
+                ans.append(0)
+                continue
+
+            cur = a
+            steps = 0
+
+            # Greedily take largest binary jumps
+            for k in range(LOG - 1, -1, -1):
+                nxt = up[k][cur]
+                if nxt < b:
+                    cur = nxt
+                    steps += 1 << k
+
+            ans.append(steps + 1)
+
+        return ans
+    
+###################################################
+# 2572. Count the Number of Square-Free Subsets
+# 13JUL26
+###################################################
+class Solution:
+    def squareFreeSubsets(self, nums: List[int]) -> int:
+        '''
+        subset of the array is square free if product of all its elements is a zqure free integer
+        sqaure free integer is an integer that is divisible by no square other than 1
+        '''
+        MOD = 10 ** 9 + 7
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # 10
+        freq = Counter(nums)
+        # consider only unique elements from nums
+        arr = list(set(nums))
+        N = len(arr) # N <= 30
+        
+        @cache
+        def rec(i, prod, empty):
+            if i == N: return not empty
+            
+            # not pick
+            cnt = rec(i + 1, prod, empty)
+
+            # check and pick
+            x = arr[i]
+            next_prod = prod * x
+            is_distinct_prime_product = all(next_prod % (prime * prime) for prime in primes)
+            if is_distinct_prime_product:
+                multiplier = freq[x] if x != 1 else (pow(2, freq[1], MOD) - 1)
+                cnt += multiplier * rec(i + 1, next_prod, 0)
+            # print(i, prod, next_prod, is_distinct_prime_product)
+
+            return cnt % MOD
+
+        return rec(0, 1, 1)
+    
+#############################################################
+# 3336. Find the Number of Subsequences With Equal GCD
+# 14JUL26
+##############################################################
+#ezzzz
+import math
+class Solution:
+    def subsequencePairCount(self, nums: List[int]) -> int:
+        '''
+        is dp to stor number of subsequnces up to i with gcd1 and gcd2
+        '''
+        n = len(nums)
+        memo = {}
+        mod = 10**9 + 7
+
+        def dp(i,gcd1,gcd2):
+            if i == n:
+                return gcd1 == gcd2
+            if (i,gcd1,gcd2) in memo:
+                return memo[(i,gcd1,gcd2)]
+            no_take_ways = dp(i+1, gcd1, gcd2)
+            take1 = dp(i+1, math.gcd(gcd1,nums[i]), gcd2)
+            take2 = dp(i+1, gcd1, math.gcd(gcd2,nums[i]))
+            ans = no_take_ways + take1 + take2
+            ans %= mod
+            memo[(i,gcd1,gcd2)] = ans
+            return ans
+        
+        return dp(0,0,0) - 1 #the null set
